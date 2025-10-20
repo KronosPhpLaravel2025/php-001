cb8c7fdb3008095b82b3ef8904dd715b5b3eb19b
fra.mansi <fra.mansi@gmail.com>
2025-10-20 12:27:54 +0200

    Update esempio001.php

--- esempio001.php
+++ esempio001.php
@@ -5,3 +5,15 @@ $variabile = "Ciao sono Francesco";
 echo $variabile; 
 
 ?>
+
+----
+
+<?php 
+$name = "Francesco";
+$city = "Bari"; 
+const SALUTO = 'Ciao'; 
+// echo $name; 
+// echo $city;
+// echo SALUTO; 
+echo SALUTO . " ". $name . " e sono di " . $city;
+?>
