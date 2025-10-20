8371f7434630098ec985b811bd4845b82a5ac3f0
fra.mansi <fra.mansi@gmail.com>
2025-10-20 13:04:56 +0200

    Comment out code in esempio001.php
    Comment out all existing code and echo statements.

--- esempio001.php
+++ esempio001.php
@@ -9,11 +9,30 @@ echo $variabile;
 ----
 
 <?php 
-$name = "Francesco";
-$city = "Bari"; 
-const SALUTO = 'Ciao'; 
+// $name = "Francesco";
+// $city = "Bari"; 
+// const SALUTO = 'Ciao'; 
 // echo $name; 
 // echo $city;
 // echo SALUTO; 
-echo SALUTO . " ". $name . " e sono di " . $city;
-?>
+//echo SALUTO . " ". $name . " e sono di " . $city;
+// echo 'SALUTO $name e sono di $city';
+// echo "SALUTO $name e sono di $city";
+
+// $a = 67;//Interi
+// $b = 3;
+// $c = 5;
+
+// $somma1 = $a + $b;//70
+// $somma2 = $somma1 + $c;//75
+
+// echo '$somma1';
+
+// $acceso = "true";//NO, usare senza apici 
+// $spento = false;
+
+// $vuoto = null;
+
+//echo "SALUTO $name e sono di $city";
+
+
