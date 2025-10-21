3b63701daf074e51ac8de23a1da92da58b0abbec
fra.mansi <fra.mansi@gmail.com>
2025-10-21 10:20:25 +0200

    Add esempio002.php with PHP array example

---
+++ esempio002.php
@@ -0,0 +1,46 @@
+<?php
+
+// $name = 'Tizio'; // Cambia, possibilita di mutare nel tempo
+// $name2 = "$name"; // Cambia, possibilita di mutare nel tempo
+
+// const GIORNO = 'Lunedi'; // NON CAMBIA
+
+
+// echo $name;
+
+
+// $numero = 14; //intero
+// $cifra = 14.89; //float
+
+// $null = null;
+
+// $array = []; //array vuoto
+// $array = ['Francesco'];
+//$array = ['Francesco', 'Ciao'];
+// $array = ['Francesco',  'test'];
+
+// $array[] = 'Carla';
+// $array[] = 'Luca';
+
+// //echo $array;
+// print_r($array);
+
+// echo "Ciao mi chiamo: " . $array[4];
+
+// $elementi = [56, 89, 67, ['lunedi', 'Rosso', ['Martedi']]];
+
+// $array = ['Test', 67, false, 'Caio', $elementi];
+
+
+// echo "Oggi è: " . $array[4][3][2][0];
+
+
+$array = [
+    'name' =>   'Francesco',
+    'city' => 'Bari',
+    78 => 'Kronos',
+    'ciao'
+
+];
+
+print_r($array);
